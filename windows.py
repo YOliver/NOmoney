bf987c83deba4e7f7d9db06bab78cafbda7b85f1
YOliver <740614279@qq.com>
2025-09-28 11:20:21 +0800

    # 开发日志 day3 模拟小丑牌，绘图模块 优化

--- windows.py
+++ windows.py
@@ -15,6 +15,11 @@ VCARD = 15
 HCARD = 15
 #点数和花色显示的行号
 SUITPOINTHOR = 1
+#牌堆数量显示行号
+DECKCNTHOR= 1
+#信息栏大小
+VIB = 5
+HIB = 50
 
 # 手牌绘制
 def PaintingHandCard(Cards):
@@ -42,8 +47,32 @@ def PaintingHandCard(Cards):
     # 界面打印
     for itemhorizontal in artboard: 
         print(itemhorizontal)
-
+# 信息打印
+def PaintingInfoBar(Cards):
+    artboard = []
+    for j in range(VIB):
+        itemhorizontal = ""
+        for i in range(HIB):
+            if j == 0 or j == VIB-1:
+                itemhorizontal = itemhorizontal+"+"
+            elif i == 0 or i == HIB-1:
+                itemhorizontal = itemhorizontal+"+"
+            else:
+                itemhorizontal = itemhorizontal+" "
+        # 绘制牌堆数量
+        if j == DECKCNTHOR:
+            strdeckcnt = str(len(Cards.deck))
+            strcemecnt = str(len(Cards.cemetery))
+            strdeckandcemecnt = "牌组:" + strdeckcnt + "   " + "墓地:" + strcemecnt
+            itemhorizontal = itemhorizontal[:2] + strdeckandcemecnt + itemhorizontal[30:]
+        if j>=len(artboard):
+            artboard.append("")
+        artboard[j]=artboard[j]+" "+itemhorizontal
+    #界面打印
+    for itemhorizontal in artboard:
+        print(itemhorizontal)
 # 主界面绘制
 def PaintingMainWindows(Cards):
     os.system('cls')
     PaintingHandCard(Cards)
+    PaintingInfoBar(Cards)
