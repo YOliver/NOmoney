b291810dcc4d23a5191d03b64c98ae4918197b18
YOliver <740614279@qq.com>
2025-10-13 17:33:45 +0800

    # 开发日志 day6 模拟小丑牌，日志模块BUG修复

--- log.py
+++ log.py
@@ -6,31 +6,38 @@
 import time
 
 log_txt_path = ".\\LOG\\"
-__private_txt_cache__ = []
-__cache_lock__ = False
+private_txt_cache = []
+cache_lock = False
 
 def log_secretary():
+    global private_txt_cache
+    global cache_lock
     while True:
         txt_name = "jocker_pocker_"+time.strftime('%Y-%m-%d-%H', time.localtime())+".log"
         log_file = open(log_txt_path+txt_name, 'w', encoding='utf-8')
         hour = time.localtime().tm_hour
         while True:
-            if len(__private_txt_cache__) > 0:
-                __cache_lock__ = True # 写日志，先锁上
+            if len(private_txt_cache) > 0:
+                while cache_lock == True:
+                    time.sleep(0.1)
+                cache_lock = True # 写日志，先锁上
                 formatted_time = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())
-                for item in __private_txt_cache__:
+                for item in private_txt_cache:
                     log_file.write(formatted_time + " DEBUG " + item + "\n")
-                __private_txt_cache__ = []
-                __cache_lock__ = False # 写完日志，解锁
+                log_file.flush()
+                private_txt_cache = []
+                cache_lock = False # 写完日志，解锁
             if hour != time.localtime().tm_hour:
                 log_file.close()
                 break
-            time.sleep(0.1)
+            time.sleep(1)
 
 def LoggerDebug(txt):
-    while __cache_lock__ == True:
+    global private_txt_cache
+    global cache_lock
+    while cache_lock == True:
         time.sleep(0.1)
-    __cache_lock__ == True
-    __private_txt_cache__.append(str(txt))
-    __cache_lock__ == False
+    cache_lock == True
+    private_txt_cache.append(str(txt))
+    cache_lock == False
 
