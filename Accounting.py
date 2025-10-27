738b87f97fbc22690532f83ffb273720329cd417
YOliver <740614279@qq.com>
2025-10-27 19:22:34 +0800

    # 开发日志 day12 模拟小丑牌，优化牌型判断函数

--- Accounting.py
+++ Accounting.py
@@ -7,46 +7,39 @@ import GlobData
 
 class accountant:
     pocker_hand_no = None
+    score = []
     def __init__(self) -> None:
         self.pocker_hand_no = GlobData.NONE
-    # 牌型判定
+    # 牌型判定+基础得分
     def HandIdentification(self, cardslist): 
         if cardslist == None or len(cardslist) == 0:
             self.pocker_hand_no = GlobData.NONE
-            return
+            return     
         suitledger=[0,0,0,0]
         pointledger=[0,0,0,0,0,0,0,0,0,0,0,0,0]
-        for item_card in cardslist:
-            suitledger[item_card.suit] = suitledger[item_card.suit] + 1
-            pointledger[item_card.point] = pointledger[item_card.point] + 1
-        self.pocker_hand_no = GlobData.HIGH #高牌
-        if 5 in suitledger:#同花
+        for card in cardslist:
+            pointledger[card.point] = pointledger[card.point] + 1
+            suitledger[card.suit] = suitledger[card.suit] + 1
+        pointstr = ''.join(str(x) for x in pointledger)
+        if '11111' in pointstr and 5 in suitledger: # 同花顺
+            self.pocker_hand_no = GlobData.STRATFLUSH
+        elif '11111' in pointstr: # 顺子
+            self.pocker_hand_no = GlobData.STRAT
+        elif 5 in suitledger: # 同花
             self.pocker_hand_no = GlobData.FLUSH
-        straightmark = 0
-        for i in pointledger:
-            if i == 4:#四条
-                self.pocker_hand_no = GlobData.FOUR
-                break
-            if i == 3:
-                if self.pocker_hand_no == GlobData.PAIR: # 葫芦
-                    self.pocker_hand_no = GlobData.FULLHS
-                    break
-                else:#三条
-                    self.pocker_hand_no = GlobData.THREE if GlobData.THREE > self.pocker_hand_no else self.pocker_hand_no
-            if i == 2:
-                if self.pocker_hand_no == GlobData.THREE:#葫芦
-                    self.pocker_hand_no = GlobData.FULLHS
-                    break
-                elif self.pocker_hand_no == GlobData.PAIR:#两对
-                    self.pocker_hand_no = GlobData.TWOP if GlobData.TWOP > self.pocker_hand_no else self.pocker_hand_no
-                else:#对子
-                    self.pocker_hand_no = GlobData.PAIR if GlobData.PAIR > self.pocker_hand_no else self.pocker_hand_no
-            if i == 1 and straightmark >= 0:
-                straightmark = straightmark + 1
-                if straightmark == 5 and self.pocker_hand_no == GlobData.FLUSH:#同花顺
-                    self.pocker_hand_no = GlobData.STRATFLUSH
-            if i == 0 and straightmark > 0 and straightmark < 5:
-                straightmark = -1
+        elif 4 in pointledger: # 四条
+            self.pocker_hand_no = GlobData.FOUR
+        elif 3 in pointledger and 2 in pointledger: # 葫芦
+            self.pocker_hand_no = GlobData.FULLHS
+        elif 3 in pointledger: # 三条
+            self.pocker_hand_no = GlobData.THREE
+        elif pointledger.count(2) == 2:# 两队
+            self.pocker_hand_no = GlobData.TWOP
+        elif 2 in pointledger: # 对子
+            self.pocker_hand_no = GlobData.PAIR
+        else: # 高牌
+            self.pocker_hand_no = GlobData.HIGH
     # 牌型记录变量回位
     def ResetHandNo(self):
         self.pocker_hand_no = GlobData.NONE
+
